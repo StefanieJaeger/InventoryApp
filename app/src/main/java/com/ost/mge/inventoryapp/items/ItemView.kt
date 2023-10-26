34afb417c70c91e7b8212ad7668c31ce2451a4a4
Laura Koch <laura.koch@ost.ch>
2023-10-26 14:10:49 +0200

    add TextField

--- app/src/main/java/com/ost/mge/inventoryapp/items/ItemView.kt
+++ app/src/main/java/com/ost/mge/inventoryapp/items/ItemView.kt
@@ -1,6 +1,8 @@
 package com.ost.mge.inventoryapp.items
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,17 +15,47 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.ost.mge.inventoryapp.data.Item
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun ItemView(navController: NavController, item: Item) {
-    Scaffold(topBar = {
-        TopAppBar(title = { Text(text = item.name) }, navigationIcon = {
-            IconButton(onClick = navController::navigateUp) {
-                Icon(Icons.Default.ArrowBack, "back")
+    var itemName by remember { mutableStateOf(item.name) }
+//    var itemDescription by remember { mutableStateOf(item.description) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = item.name) },
+                navigationIcon = {
+                    IconButton(onClick = navController::navigateUp)
+                    {
+                        Icon(Icons.Default.ArrowBack, "back")
+                    }
+                }
+            )
+        }
+    ) /*{ padding ->*/
+//        SelectionContainer {
+//            Text(text = "This is item ${item.name}", Modifier.padding(padding))
+//        }
+    { innerpadding ->
+        Box {
+            TextField(
+//                label = { Text(text= "Itemname")},
+                value = itemName,
+                onValueChange = { itemName = it },
+                Modifier.padding(innerpadding)
+            )
+            if (item.name.isEmpty()) {
+                Text(text = "Placeholder")
             }
-        })
-    }) { padding ->
-        Text(text = "This is item ${item.name}", Modifier.padding(padding))
+        }
     }
 }
