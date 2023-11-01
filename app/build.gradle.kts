b723bd99807d77f6aaf7feab8592b58e1cc02cf8
Laura Koch <laura.koch@ost.ch>
2023-11-01 17:19:21 +0100

    add buildConfig and coil for imagecapturing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -79,4 +80,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
 }
