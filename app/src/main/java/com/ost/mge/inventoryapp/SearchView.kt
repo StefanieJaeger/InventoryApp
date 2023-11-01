95b77e9592c5960a9b29565cdd8d98a88dfad2fe
Stefanie Jäger <stefijaeger@gmail.com>
2023-11-01 10:43:09 +0100

    Fix search!

--- app/src/main/java/com/ost/mge/inventoryapp/SearchView.kt
+++ app/src/main/java/com/ost/mge/inventoryapp/SearchView.kt
@@ -1,69 +1,51 @@
-package com.ost.mge.inventoryapp
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.TextField
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchView(searchState: MutableState<TextFieldValue>) {
-    TextField(
-        value = searchState.value,
-        onValueChange = {value -> searchState.value = value },
-        modifier = Modifier.fillMaxWidth(),
-        singleLine = true,
-        shape = RectangleShape,
-        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
-        leadingIcon = {
-            Icon(
-                Icons.Default.Search,
-                contentDescription = "Search categories",
+fun SearchView(searchText: MutableState<String>, getSuggestions: (String) -> List<String>) {
+    var active by rememberSaveable { mutableStateOf(false) }
+
+    SearchBar(
+        modifier = Modifier,
+        query = searchText.value,
+        onQueryChange = {searchText.value = it},
+        onSearch = { active = false },
+        active = active,
+        onActiveChange = {
+            active = it
+        },
+        placeholder = { Text("Search the list") },
+        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+        trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+    ) {
+        val suggestions = getSuggestions(searchText.value)
+        repeat(suggestions.size) { idx ->
+            val text = suggestions[idx]
+            ListItem(
+                headlineContent = { Text(text) },
                 modifier = Modifier
-                    .padding(15.dp)
-                    .size(22.dp)
-            )
-        },
-        trailingIcon = {
-            if (searchState.value != TextFieldValue("")) {
-                IconButton(
-                    onClick = {
-                        searchState.value =
-                            TextFieldValue("")
+                    .clickable {
+                        searchText.value = text
+                        active = false
                     }
-                ) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = "Clear search",
-                        modifier = Modifier
-                            .padding(15.dp)
-                            .size(24.dp)
-                    )
-                }
-            }
-        },
-//        colors = TextFieldDefaults.textFieldColors(
-//            textColor = Color.White,
-//            cursorColor = Color.White,
-//            leadingIconColor = Color.White,
-//            trailingIconColor = Color.White,
-//            backgroundColor = colorResource(id = R.color.colorPrimary),
-//            focusedIndicatorColor = Color.Transparent,
-//            unfocusedIndicatorColor = Color.Transparent,
-//            disabledIndicatorColor = Color.Transparent
-//        )
-    )
+                    .fillMaxWidth()
+            )
+        }
+    }
 }
--- app/src/main/java/com/ost/mge/inventoryapp/categories/CategoriesView.kt
+++ app/src/main/java/com/ost/mge/inventoryapp/categories/CategoriesView.kt
@@ -1,5 +1,6 @@
 package com.ost.mge.inventoryapp.categories
 
+import SearchView
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -29,21 +29,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.ost.mge.inventoryapp.DeleteAction
 import com.ost.mge.inventoryapp.EditAction
-import com.ost.mge.inventoryapp.SearchView
+import com.ost.mge.inventoryapp.InlineTextField
 import com.ost.mge.inventoryapp.data.Category
 import kotlin.math.roundToInt
 
@@ -57,13 +58,14 @@ enum class DragAnchors {
     @OptIn(ExperimentalMaterial3Api::class)
     fun CategoriesView(
     categories: List<Category>,
-    navController: NavHostController
+    navController: NavHostController,
+    onEditCategory: (Category) -> Unit,
+    onDeleteCategory: (Category) -> Unit
     ) {
-    val searchState = remember { mutableStateOf(TextFieldValue("")) }
+    val searchText = remember { mutableStateOf("") }
     var filteredCategories = categories
-    if(searchState.value.text.isNotEmpty()) {
-        // todo: throws when entering only number
-        filteredCategories = categories.filter { it.name.contains(searchState.value.text, ignoreCase = true) }
+    if(searchText.value.isNotEmpty()) {
+        filteredCategories = categories.filter {it.name.contains(searchText.value, ignoreCase = true)}
     }
 
     Scaffold(
@@ -77,12 +79,21 @@ enum class DragAnchors {
         }
         )  { padding ->
         Column(Modifier.padding(padding)) {
-            SearchView(searchState)
+            SearchView(searchText) { text ->
+                categories.filter {
+                    it.name.contains(
+                        text,
+                        ignoreCase = true
+                    )
+                }.map { it.name }
+            }
             LazyColumn {
                 items(filteredCategories) { category ->
                     DraggableCategoryListItem(
                         category,
-                        onItemClick = {navController.navigate("categories/${category.id}/items")}
+                        onCategoryClick = {navController.navigate("categories/${category.id}/items")},
+                        onEditCategory,
+                        onDeleteCategory
                     )
                 }
             }
@@ -91,17 +102,26 @@ enum class DragAnchors {
 }
 
 @Composable
-fun DraggableCategoryListItem(category: Category, onItemClick: (Category) -> Unit) {
-    Row(Modifier.clickable(onClick = { onItemClick(category) })) {
-        DraggableItem {
-            Box(Modifier
-                // todo: use our themed color
-                .background(Color.Green)
-                .fillMaxSize(),
+fun DraggableCategoryListItem(category: Category, onCategoryClick: (Category) -> Unit, onEditCategory: (Category) -> Unit, onDeleteCategory: (Category) -> Unit) {
+    var editMode by remember {
+        mutableStateOf(false)
+    }
+    // todo: reset swipe on edit or delete
+    Row(Modifier.clickable(onClick = { onCategoryClick(category) })) {
+        DraggableItem (onEdit = {editMode = true}, onDelete = {onDeleteCategory(category)}) {
+            Box(
+                Modifier
+                    // todo: use our themed color
+                    .background(Color.Green)
+                    .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
                 // todo: style list item nicely
-                Text(category.name)
+                if(editMode) {
+                    InlineTextField(category.name, onDone = {category.name = it; onEditCategory(category); editMode = false})
+                } else {
+                    Text(category.name)
+                }
             }
         }
     }
@@ -110,7 +130,7 @@ fun DraggableCategoryListItem(category: Category, onItemClick: (Category) -> Uni
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DraggableItem(
-    content: @Composable BoxScope.() -> Unit
+    onEdit: () -> Unit, onDelete: () -> Unit, content: @Composable BoxScope.() -> Unit
 ) {
     val density = LocalDensity.current
     val defaultActionSize = 80.dp
@@ -129,6 +149,16 @@ fun DraggableItem(
         )
     }
 
+    val editAction = {
+        // todo: reset drag!
+        onEdit()
+    }
+
+    val deleteAction = {
+        // todo: reset drag!
+        onDelete()
+    }
+
     Box(
         modifier = Modifier
             .padding(16.dp)
@@ -139,16 +169,15 @@ fun DraggableItem(
             Modifier.align(Alignment.CenterEnd),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            // todo: remove item or let user edit text
             EditAction(
                 Modifier
                     .width(defaultActionSize),
-                {}
+                editAction
             )
             DeleteAction(
                 Modifier
                     .width(defaultActionSize),
-                {}
+                deleteAction
             )
         }
 
